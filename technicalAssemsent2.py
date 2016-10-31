74297f96630b2a29dfdcd73d095e2101e272524f
Vincent Gentile <DragonSlayer@dhcp-v091-074.mobile.uci.edu>
2016-10-31 15:04:52 -0700

    File for step 2 of technical assesment

---
+++ technicalAssemsent2.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+first_endpoint = 'http://challenge.code2040.org/api/reverse'
+second_endpoint = 'http://challenge.code2040.org/api/reverse/validate'
+token = 'd6e8cc3d628276b7416906ce28774a6a'
+
+data = {'token' : token}
+head = {'content-type': "application/json"}
+
+request = requests.post(first_endpoint, data = json.dumps(data), headers = head)
+
+#print request.text
+
+textToReverse = request.text
+reverseText = textToReverse[::-1]
+
+reversedData = {'token' : token, 'string' : reverseText}
+
+validate_request = requests.post(second_endpoint, data = json.dumps(reversedData), headers = head)
+
+print validate_request.text
+
